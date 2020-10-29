0a5e1a4f7050af96f823c23d59bb69e7e1f1809e
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2020-10-29 20:08:46 +1100

    Add files via upload

--- index.php
+++ index.php
@@ -15,14 +15,40 @@
 <html>
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<!--link rel="stylesheet" type="text/css" href="style.css"-->
+<style>
+.header{
+	text-align:center;
+}
+
+a:link {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+a:active {
+  text-decoration: underline;
+}
+
+.content{
+	text-align:center;
+}
+</style>
+
 </head>
 <body>
 
-<div class="header">
+<div class="header" style="center">
 	<h2>Home Page</h2>
 </div>
-<div class="content">
+<div class="content" style="center">
   	<!-- notification message -->
   	<?php if (isset($_SESSION['success'])) : ?>
       <div class="error success" >
@@ -40,6 +66,8 @@
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
     	<p><a href="Add_Biller_Information.php">Add Biller Information</a></p>
     	<p><a href="booking.php">Book an appointment</a></p>
+    	<p><a href="user_records.php">Records</a></p>
+    	<p><a href="update_personal_information.php">Update personal information</a></P>
     	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
